f62059e885af7680f413f72202a3f8c7c7873449
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-10-31 10:30:28 -0400

    update eula script

--- modify-eula.js
+++ modify-eula.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
 
+var eula = fs.readFileSync("./eula.txt", { encoding: "utf8" });
+
 // For testing
-console.log(fs.readFileSync("./eula.txt", { encoding: "utf8" }));
+console.log(eula);
+
+fs.writeFileSync("./eula.txt", eula.replaceAll("false", "true"));
