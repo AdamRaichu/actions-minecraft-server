bc65a6961a627bc61ba065bf2a353266a4a7b90d
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-10-31 02:08:37 +0000

    add fabric server jar

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ chmod u+x ./tunnels-status.sh
 ./install-playit.sh
 ./tunnels-status.sh
 
-playit --secret $PLAYIT_SECRET this-donations.gl.joinmc.link
+playit --secret $PLAYIT_SECRET this-donations.gl.joinmc.link &
+java -Xmx2G -jar fabric-server-mc.1.20.2-loader.0.14.24-launcher.0.11.2.jar nogui
