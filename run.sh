b3b27338a3dc6c9f9d64b0b348e2aeeced366e3b
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-10-31 02:09:28 +0000

    enable up to 6G of RAM

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ chmod u+x ./tunnels-status.sh
 ./tunnels-status.sh
 
 playit --secret $PLAYIT_SECRET this-donations.gl.joinmc.link &
-java -Xmx2G -jar fabric-server-mc.1.20.2-loader.0.14.24-launcher.0.11.2.jar nogui
+java -Xmx6G -jar fabric-server-mc.1.20.2-loader.0.14.24-launcher.0.11.2.jar nogui
