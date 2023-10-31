adf097ff0411de7d0bf6c172b970d38561e0cfc7
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-10-31 10:00:34 -0400

    add modify-eula.js
    add server-start template

---
+++ modify-eula.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+
+// For testing
+console.log(fs.readFileSync("./eula.txt", { encoding: "utf8" }));
--- run.sh
+++ run.sh
@@ -4,5 +4,10 @@ chmod u+x ./tunnels-status.sh
 ./install-playit.sh
 ./tunnels-status.sh
 
+# Run the first time in order to generate eula.txt
+java -Xmx6G -jar fabric-server-mc.1.20.2-loader.0.14.24-launcher.0.11.2.jar nogui
+
+node ./modify-eula.js
+
 playit --secret $PLAYIT_SECRET this-donations.gl.joinmc.link &
 java -Xmx6G -jar fabric-server-mc.1.20.2-loader.0.14.24-launcher.0.11.2.jar nogui
