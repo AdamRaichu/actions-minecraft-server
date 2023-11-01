8616a21289a77b176eefaf33307b55cf70cb95f6
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-11-01 22:48:09 +0000

    run replacement before console logging

--- modify-eula.js
+++ modify-eula.js
@@ -2,7 +2,9 @@ const fs = require("fs");
 
 var eula = fs.readFileSync("./eula.txt", { encoding: "utf8" });
 
+eula = eula.replaceAll("false", "true");
+
 // For testing
 console.log(eula);
 
-fs.writeFileSync("./eula.txt", eula.replaceAll("false", "true"));
+fs.writeFileSync("./eula.txt", eula);
--- modify-server-prop.js
+++ modify-server-prop.js
@@ -2,7 +2,9 @@ const fs = require("fs");
 
 var serverProp = fs.readFileSync("./server.properties", { encoding: "utf8" });
 
+serverProp = serverProp.replaceAll("enable-query=false", "enable-query=true").replaceAll("motd=A Minecraft Server", "motd=A Minecraft server running in GitHub Actions");
+
 // For testing
 console.log(serverProp);
 
-fs.writeFileSync("./server.properties", serverProp.replaceAll("enable-query=false", "enable-query=true").replaceAll("motd=A Minecraft Server", "motd=A Minecraft server running in GitHub Actions"));
+fs.writeFileSync("./server.properties", serverProp);
