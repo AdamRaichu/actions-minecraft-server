6654aa55dbb0d2ed67abe85f2af3f56ad4884a29
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-11-01 20:01:16 -0400

    revert script

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ node ./modify-eula.js
 node ./modify-server-prop.js
 
 printf 'op AdamRaichu' | java -Xmx6G -jar fabric-server-mc.1.20.2-loader.0.14.24-launcher.0.11.2.jar nogui &
-playit # --secret $PLAYIT_SECRET run
+playit --secret $PLAYIT_SECRET run
